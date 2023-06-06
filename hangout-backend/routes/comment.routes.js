6d329ecdd96629476548286739a9887be2c64ed3
avelarvicky <victoriahelenaavelar@gmail.com>
2023-06-06 14:57:12 +0100

    mvp done

--- hangout-backend/routes/comment.routes.js
+++ hangout-backend/routes/comment.routes.js
@@ -5,18 +5,18 @@ const Hangout = require("../models/Hangout.model");
 const Comment = require("../models/Comment.model");
 
 // POST '/api/comments' route to create a new comment
-router.post("/:id/comments", async (req, res) => {
-	const { content, hangoutId } = req.body;
-	const { id } = req.params;
+router.post("/:hangoutId/comments", async (req, res) => {
+	const { content } = req.body;
+	const { hangoutId } = req.params;
 	// const user  = req.payload._id;
 	try {
 		// create a new comment
 		let newComment = await Comment.create({
-			content,
+			content
 		});
 
 		// push new comment to hangout
-		let hangoutComment = await Hangout.findByIdAndUpdate(id, {
+		let hangoutComment = await Hangout.findByIdAndUpdate(hangoutId, {
 			$push: { comments: newComment._id },
 		});
 
@@ -31,34 +31,19 @@ router.post("/:id/comments", async (req, res) => {
 });
 
 // GET /api/comments route to get comments of a specific hangout
-router.get("/:id/comments", async (req, res) => {
-	const { id } = req.params;
+router.get("/:hangoutId/comments", async (req, res) => {
+	const { hangoutId } = req.params;
 
 	try {
-		let hangout = await Hangout.findById(id);
-		
-		const hangoutPopulate = (populatedHangout) => {
-			let arr = [];
-			populatedHangout.comments.map( async (comment)=> {
-				let kevin = await populatedHangout.populate(comment)
-				arr.push(kevin)
-			})
-			console.log(arr)
-		}
-
-		hangout.populate();
-
-		/* let arr = [];
-		hangout.comments.map((comment)=> {
-			arr.push(comment.populate())
-		}) */
-
-		console.log(hangout);
-	} catch (error) {}
+		let foundHangout = await Hangout.findById(hangoutId).populate("comments");
+		res.json(foundHangout.comments);
+	} catch (error) {
+		res.json(error);
+	}
 });
 
 // GET /api/hangouts/:hangoutId to get details of a specific comment
-router.get("/comments/:commentId", async (req, res) => {
+router.get("/:hangoutId/comments/:commentId", async (req, res) => {
 	const { commentId } = req.params;
 
 	if (!mongoose.Types.ObjectId.isValid(commentId)) {
@@ -75,7 +60,7 @@ router.get("/comments/:commentId", async (req, res) => {
 });
 
 // PUT '/api/comments/:commentId' route to edit comment
-router.put("/comments/:commentId", async (req, res) => {
+router.put("/:hangoutId/comments/:commentId", async (req, res) => {
 	const { commentId } = req.params;
 	const { content } = req.body;
 
@@ -97,7 +82,7 @@ router.put("/comments/:commentId", async (req, res) => {
 });
 
 // DELETE '/api/comments/:commentId' route to delete comment
-router.delete("/comments/:commentId", async (req, res) => {
+router.delete("/:hangoutId/comments/:commentId", async (req, res) => {
 	const { commentId } = req.params;
 
 	if (!mongoose.Types.ObjectId.isValid(commentId)) {
